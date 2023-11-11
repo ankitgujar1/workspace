3f9ea6ed37fc563ee239a63f0dc9338eb73babda
Iamneo <techadmin@iamneo.ai>
2023-11-11 06:36:39 +0000

    Backup Commit

--- angularapp/src/app/crud/crud.component.ts
+++ angularapp/src/app/crud/crud.component.ts
@@ -1,67 +1,61 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-crud',
-  templateUrl: './crud.component.html',
-  styleUrls: ['./crud.component.css']
-})
-export class CrudComponent {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-  indiData: object = { email: " ", password: " ", isReady: false };
-  userData: object[] = [{}];
-
-  dataHandler(event) {
-    let ids: string = event.target.id;
-    let data: any = event.target.value;
-
-    switch (ids) {
-      case "emId":
-        this.indiData = { ...this.indiData, email: data };
-        break;
-      case "psId":
-        this.indiData = { ...this.indiData, password: data };
-        break;
-      case "chId":
-        let chkVal = event.target.checked
-        this.indiData = { ...this.indiData, isReady: chkVal };
-        break;
-      case "btn1":
-
-        // console.log(this.indiData);
-        this.userData.push(this.indiData);
-        // // this.userData.push(this.userData);
-        console.log(this.indiData);
-        // console.log("aaaa");
-        break;
-
-      case "upbtn":
-        // this.userData.forEach((val,indx,arr)=>{
-        //   if(val.email==this.indiData.email){
-        //     console.log(val);
-        //   }
-        // });
-
-        this.userData.forEach((value,indx,arr)=>{
-          if(value.email==this.indiData.email){
-              console.log("same");
-          }
-      })
-
-      // let value = this.userData.find(x=>x.email == this.indiData.email) ;
-      //           console.log(value);
-               
+import { Component } from "@angular/core";
  
-                 this.userData.push(this.indiData)
-        break;
-      default:
-        break;
-
-    }
-    console.log(event.target.id);
+ 
+@Component({
+    selector:'app-crud',
+    templateUrl:'./crud.component.html'
+})
+export class CrudComponent
+{
+    indiData:object = {email:" ",password:" ",isReady:false};
+    userData:object[]=[{email:" ",password:" ",isReady:false}];
+ 
+    dataHandler(event){
+       let ids:string = event.target.id;
+       let data:any = event.target.value;
+       
+        switch (ids) {
+            case "emId":
+             this.indiData = {...this.indiData,email:data};
+             if(this.userData.length != 0){
+             let value =this.userData.find(x=>x.email == this.indiData.email);
+ 
+           
+             }
+                break;
+                case "psId":
+                    this.indiData={...this.indiData,password: data}
+                break;
+                case "chId":
+                   let chkVal = event.target.checked;
+                    this.indiData={...this.indiData,isReady: chkVal}
+                break;
+               
+                case "btn1":
+                 
+                 this.userData.push(this.indiData);
+               
+                break;
+                case "upbtn":
+                 
+                // this.userData.forEach((value,indx,arr)=>{
+                //   if(value.email == this.indiData.email){
+                   
+                //   }
+                // });
+                this.userData.forEach((value,indx,arr)=>{
+                    if(value.email==this.indiData.email){
+                        console.log("same");
+                    }
+                })
+                 //console.log(value);
+                 this.userData.push(this.indiData);
+               
+                break;
+       
+            default:
+                break;
+        }
+        console.log(event.target.id);
   }
-
 }
