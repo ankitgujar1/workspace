167bb67735961a24d01c486d2a45ffb9ead0d419
Iamneo <techadmin@iamneo.ai>
2023-11-11 05:12:03 +0000

    Backup Commit

--- angularapp/src/app/crud/crud.component.ts
+++ angularapp/src/app/crud/crud.component.ts
@@ -5,51 +5,51 @@ import { Component } from '@angular/core';
   templateUrl: './crud.component.html',
   styleUrls: ['./crud.component.css']
 })
-export class CrudComponent  {
+export class CrudComponent {
 
   constructor() { }
 
   ngOnInit() {
   }
-  indiData:object={email:" ",password:" ",isReady:false};
-  userData:object[]=[{email:" ",password:" ",isReady:false}];
+  indiData: object = { email: " ", password: " ", isReady: false };
+  userData: object[] = [{ email: " ", password: " ", isReady: false }];
 
-  dataHandler(event){
-      let ids:string  = event.target.id;
-      let data:any = event.target.value;
+  dataHandler(event) {
+    let ids: string = event.target.id;
+    let data: any = event.target.value;
 
-  switch (ids)                                      
-  {
-    case "emId":
-      this.indiData={...this.indiData,email:data};
-      break;
-    case "psId":
-      this.indiData={...this.indiData,password:data};
-      break;
-    case "chId":
-      let chkVal = event.target.checked
-      this.indiData={...this.indiData,isReady: chkVal};
-      break;
-    case "btn1":
+    switch (ids) {
+      case "emId":
+        this.indiData = { ...this.indiData, email: data };
+        break;
+      case "psId":
+        this.indiData = { ...this.indiData, password: data };
+        break;
+      case "chId":
+        let chkVal = event.target.checked
+        this.indiData = { ...this.indiData, isReady: chkVal };
+        break;
+      case "btn1":
 
-      // console.log(this.indiData);
-       this.userData.push(this.indiData);
-      // // this.userData.push(this.userData);
-      // console.log(this.userData);
-      // console.log("aaaa");
-      break;
+        // console.log(this.indiData);
+        this.userData.push(this.indiData);
+        // // this.userData.push(this.userData);
+        console.log(this.indiData);
+        // console.log("aaaa");
+        break;
 
       case "upbtn":
-        // let value = this.userData.find(x=>x.email == this.indiData.email)  
-        //         console.log(value);
+        this.userData.forEach((val,indx,arr)=>{
+          if(val.email==this.indiData.email){
+            console.log(val);
+          }
+        });
+        break;
+      default:
+        break;
 
-        
-      break;
-    default:
-      break;
-   
-  }
-  console.log(event.target.id);
+    }
+    console.log(event.target.id);
   }
 
 }
