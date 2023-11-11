5ba7c383f5cd4111ad8a356722a497534d090dca
Iamneo <techadmin@iamneo.ai>
2023-11-11 08:51:40 +0000

    Backup Commit

--- angularapp/src/app/crud/crud.component.ts
+++ angularapp/src/app/crud/crud.component.ts
@@ -7,55 +7,6 @@ import { Component } from "@angular/core";
 })
 export class CrudComponent
 {
-    indiData:object = {email:" ",password:" ",isReady:false};
-    userData:object[]=[{email:" ",password:" ",isReady:false}];
- 
-    dataHandler(event){
-       let ids:string = event.target.id;
-       let data:any = event.target.value;
-       
-        switch (ids) {
-            case "emId":
-             this.indiData = {...this.indiData,email:data};
-             if(this.userData.length != 0){
-             let value =this.userData.find(x=>x.email == this.indiData.email);
- 
-           
-             }
-                break;
-                case "psId":
-                    this.indiData={...this.indiData,password: data}
-                break;
-                case "chId":
-                   let chkVal = event.target.checked;
-                    this.indiData={...this.indiData,isReady: chkVal}
-                break;
-               
-                case "btn1":
-                 
-                 this.userData.push(this.indiData);
-               
-                break;
-                case "upbtn":
-                 
-                // this.userData.forEach((value,indx,arr)=>{
-                //   if(value.email == this.indiData.email){
-                   
-                //   }
-                // });
-                this.userData.forEach((value,indx,arr)=>{
-                    if(value.email==this.indiData.email){
-                        console.log("same");
-                    }
-                })
-                 //console.log(value);
-                 this.userData.push(this.indiData);
-               
-                break;
-       
-            default:
-                break;
-        }
-        console.log(event.target.id);
-  }
+    date=new Date();
+    n:number=1000000;
 }
