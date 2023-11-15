e5f75f542328a262588f9818c78244aee5000bd1
Iamneo <techadmin@iamneo.ai>
2023-11-15 10:06:10 +0000

    Backup Commit

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -3,16 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 // import { Routes } from '@angular/router';`
 
 import { TemplateformComponent } from './templateform/templateform.component';
-// import { FirstComponent } from './first/first.component';
+import { FirstComponent } from './first/first.component';
 import { HomeComponent } from './home/home.component';
 import { CrudComponent } from './crud/crud.component';
+import { HttpComponent } from './http/http.component';
 
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'form/:id',component:TemplateformComponent},
   {path:'home',component:HomeComponent},
-  {path:'pipe',component:CrudComponent}
+  {path:'pipe',component:CrudComponent},
+  {path:'http',component:HttpComponent},
+  {path:'first',component:FirstComponent}
 ];
 
 @NgModule({
