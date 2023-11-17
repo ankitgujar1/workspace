2e2c6d299e5b4fcee7b080c9a8f4de3c3c33d781
Iamneo <techadmin@iamneo.ai>
2023-11-17 12:06:32 +0000

    Backup Commit

--- angularapp/src/app/backend.service.ts
+++ angularapp/src/app/backend.service.ts
@@ -12,75 +12,87 @@ import { Observable } from 'rxjs';
 })
 export class BackendService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  SaveData(data:any){ //post
-    let responseData:any="";
-    let httpHeaders:HttpHeaders=new HttpHeaders({
-      Accept:"application/json"
+  SaveData(data: any) { //post
+    let responseData: any = "";
+    let httpHeaders: HttpHeaders = new HttpHeaders({
+      Accept: "application/json"
     })
 
-    this.http.post("https://8081-abbcbfeabdfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/api/Employee",data,{headers:httpHeaders})
-    .subscribe(res=>{
-      responseData=res;
+    this.http.post("https://8081-abbcbfeabdfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/api/Employee", data, { headers: httpHeaders })
+      .subscribe(res => {
+        responseData = res;
 
-    },
-    error=>{
-      responseData=error;
-    })
+      },
+        error => {
+          responseData = error;
+        })
     return responseData;
   }
 
-  UpdateData(id:number,data:any){ //put
-    let responseData:any="";
-    let httpHeaders:HttpHeaders=new HttpHeaders({
-      Accept:"application/json"
+  UpdateData(id: number, data: any) { //put
+    let responseData: any = "";
+    let httpHeaders: HttpHeaders = new HttpHeaders({
+      Accept: "application/json"
     })
-    this.http.put("https://8081-abbcbfeabdfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/api/Employee/"+id,data,{headers:httpHeaders})
-    .subscribe(res=>{
-      responseData=res;
+    this.http.put("https://8081-abbcbfeabdfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/api/Employee/" + id, data, { headers: httpHeaders })
+      .subscribe(res => {
+        responseData = res;
 
-    },
-    error=>{
-      responseData=error;
-    })
+      },
+        error => {
+          responseData = error;
+        })
     return responseData;
   }
 
-  DeleteData(id:number){ //delete
-    let responseData:any="";
-    let httpHeaders:HttpHeaders=new HttpHeaders({
-      Accept:"application/json"
+  DeleteData(id: number) { //delete
+    let responseData: any = "";
+    let httpHeaders: HttpHeaders = new HttpHeaders({
+      Accept: "application/json"
     })
-    this.http.delete("https://8081-abbcbfeabdfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/api/Employee/"+id,{headers:httpHeaders})
-    .subscribe(res=>{
-      responseData=res;
+    this.http.delete("https://8081-abbcbfeabdfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/api/Employee/" + id, { headers: httpHeaders })
+      .subscribe(res => {
+        responseData = res;
 
-    },
-    error=>{
-      responseData=error;
-    })
+      },
+        error => {
+          responseData = error;
+        })
     return responseData;
   }
 
-  
-  GetData() : Observable<IEmployee[]>{
-    let responceData:any="";
-    let httpHeaders:HttpHeaders = new HttpHeaders({
-      Accept:'application/json'
+
+  GetData(): Observable<IEmployee[]> {
+    let responceData: any = "";
+    let httpHeaders: HttpHeaders = new HttpHeaders({
+      Accept: 'application/json'
     })
-     return this.http.get<IEmployee[]>("https://8081-abbcbfeabdfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/api/Employee",{headers:httpHeaders})
-   
+    return this.http.get<IEmployee[]>("https://8081-abbcbfeabdfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/api/Employee", { headers: httpHeaders })
+
   }
 
-  GetDataById(id:number){ //retrive by id
-    
+  GetDataById(id: number) { //retrive by id https://8081-abbcbfeabdfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/api/Employee/8
+    let responseData: any = "";
+    let httpHeaders: HttpHeaders = new HttpHeaders({
+      Accept: 'application/json'
+    })
+    this.http.get("https://8081-abbcbfeabdfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/api/Employee/" + id, { headers: httpHeaders })
+      .subscribe(res => {
+        responseData = res;
+
+      },
+        error => {
+          responseData = error;
+        })
+    return responseData;
   }
 }
 
-export interface IEmployee{
-  id:number;
-  name:string;
-  salary:number;
-  joinDate:string;
+export interface IEmployee {
+  id: number;
+  name: string;
+  salary: number;
+  joinDate: string;
 }
--- angularapp/src/app/crud-operations/crud-operations.component.ts
+++ angularapp/src/app/crud-operations/crud-operations.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { BackendService } from '../backend.service';
-import{IEmployee} from '../backend.service'
+import { IEmployee } from '../backend.service'
 
 @Component({
   selector: 'app-crud-operations',
@@ -9,29 +9,37 @@ import{IEmployee} from '../backend.service'
   styleUrls: ['./crud-operations.component.css']
 })
 export class CrudOperationsComponent implements OnInit {
-  listData:IEmployee[]=[]
-  constructor(private http:BackendService) {
-    http.GetData().subscribe(success=>{
+  listData: IEmployee[] = []
+
+  listDataById: IEmployee[] = []
+
+  constructor(private http: BackendService) {
+    http.GetData().subscribe(success => {
       this.listData = success;
-  })
+    })
   }
 
   ngOnInit() {
   }
 
 
-  onSubmitPost(postForm:NgForm){
-    
+  onSubmitPost(postForm: NgForm) {
+
     this.http.SaveData(postForm.value);
   }
 
-  onSubmitPut(putForm:NgForm){
+  onSubmitPut(putForm: NgForm) {
     // console.log(putForm.value)
-    this.http.UpdateData(putForm.value.id,putForm.value);
+    this.http.UpdateData(putForm.value.id, putForm.value);
   }
 
-  onSubmitDelete(deleteForm:NgForm){
+  onSubmitDelete(deleteForm: NgForm) {
     this.http.DeleteData(deleteForm.value.id);
+  }
+
+  onSubmitgetById(getByIdForm:NgForm){
+    // console
+    this.listDataById= this.http.GetDataById(getByIdForm.value.id);
   }
 
 }
